b31ccfb37b78230ee6b5ffd01ea0da03a2f17f80
Mahdi Fahimi <ma.79.fahimi@gmail.com>
2022-11-17 21:25:59 +0330

    blogs component created

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './App.css';
 //import components 
 import Header from '../src/components/Header/Header'
 import Hero from './components/Hero/Hero'
+import Blogs from './components/Blogs/Blogs'
 
 function App() {
   return (
     <div className="">
 
     <Header /> 
-
     <Hero />
+    <Blogs/>
     
 
     </div>
