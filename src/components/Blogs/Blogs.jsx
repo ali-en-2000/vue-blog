e411c0f89b675b9f05a27051b303ac6319ef6e96
Mahdi Fahimi <ma.79.fahimi@gmail.com>
2022-11-17 21:26:34 +0330

    blogs component created

---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Blogs.css'
+
+const Blogs = () => {
+  return (
+    <div className="blogs container mt-4">
+      test text  
+    </div>
+  )
+}
+
+export default Blogs
