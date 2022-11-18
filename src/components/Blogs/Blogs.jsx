b2f6c808cd4124dd3c1b29b056296ec6b6c6994d
Mahdi Fahimi <ma.79.fahimi@gmail.com>
2022-11-18 15:34:48 +0330

    finnish right side of blogs component

--- src/components/Blogs/Blogs.jsx
+++ src/components/Blogs/Blogs.jsx
@@ -13,7 +13,7 @@ const Blogs = (props) => {
             {
               blogData.posts.map((item) => (
                 <div className="card m-4" style={{width: "45%"}} >
-                  <img src={item.photo} className="card-img-top" alt="..." />
+                  <img src={item.photo} className="card-img-top w-100" alt="..." />
                   <div className="card-body">
                     <h5 className="card-title">{item.title}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
