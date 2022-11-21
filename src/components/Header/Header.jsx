d6b5a898f2dc2fcbce286aeffb111f0652c7d8a0
Mahdi Fahimi <ma.79.fahimi@gmail.com>
2022-11-21 20:29:26 +0330

    start to complite header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,12 +8,16 @@ const Header = () => {
     <div className="header py-2 ">
 
       <div className="header-container container">
-        <ul className="nav d-flex align-center justify-between">
-          <li><a href="#" > خانه </a></li>
-          <li><a href="#" > درباره ما </a></li>
-          <li><a href="#" > تماس با ما </a></li>
-        </ul>
-        <div className="logo h1 "> LOGO </div>
+        <div className="header-right-side">
+          <ul className="nav d-flex align-center justify-between">
+            <li><a href="#" > خانه </a></li>
+            <li><a href="#" > درباره ما </a></li>
+            <li><a href="#" > تماس با ما </a></li>
+          </ul>
+        </div>
+        <div className="header-left-side">
+          <div className="logo h1 "> LOGO </div>
+        </div>
       </div>
 
 
