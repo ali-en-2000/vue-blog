f6df943dd4e21f2f55c7195f451924694e5b260d
Mahdi Fahimi <ma.79.fahimi@gmail.com>
2022-11-22 15:22:00 +0330

    responsive header complited

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,26 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Header.css'
 import { Link } from 'react-router-dom'
 import { FiMenu, FiSearch, FiUser } from "react-icons/fi";
 
 
-
-
 const Header = () => {
+
+  const [showNav, setShowNav] = useState(false);
+
   return (
-    <div className="header py-2 ">
+    <div className="header py-2 mt-2">
 
       <div className="header-container container">
 
         <div className="header-right-side d-flex align-items-center justify-between">
           <div className="logo h1 ms-3"> LOGO</div>
-          <ul className="nav d-flex align-items-center justify-between">
+          {/* <ul className="nav d-flex align-items-center justify-between"> */}
+          <ul className={
+            showNav ?
+            "nav d-flex align-items-center justify-between" :
+            "nav d-flex align-items-center justify-between d-none"
+          }>
             <li>خانه </li>
             <li>درباره ما </li>
             <li>تماس با ما </li>
@@ -23,12 +29,12 @@ const Header = () => {
 
         <div className="header-left-side d-flex align-items-center justify-between">
 
-          <div id="logo-in">
+          <div id="logo-in"  >
             <span> <FiUser /> </span>
             <span> ورود </span>
           </div>
 
-          <div className="menu d-block d-md-none ">
+          <div className="menu d-block d-md-none " onClick={() => setShowNav(!showNav)}>
             <FiMenu className="h2 " />
           </div>
 
