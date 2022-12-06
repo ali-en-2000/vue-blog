0321cf87b1a9dfd216878d15afbe0da02af1acbf
Ali reza <a.rezamohmmadi369@gmail.com>
2022-12-06 23:04:58 +0330

    add tailwind css

---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,vue,tsx}",
+  ],  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
