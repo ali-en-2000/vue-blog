be32e062117b53c682ebde7f0e0b97eba424ca41
Ali reza <a.rezamohmmadi369@gmail.com>
2022-12-08 02:55:50 +0330

    bug fix

---
+++ src/View/Blog/index.vue
@@ -0,0 +1,66 @@
+<script setup>
+import axios from 'axios'
+import { reactive, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
+import { useRouter } from 'vue-router'
+const router = useRouter();
+
+
+
+
+
+var state = reactive({
+  blog: null
+});
+onMounted(() => {
+  const route = useRoute()
+  console.log(route.params.id)
+  getBlog(route.params.id)
+
+})
+
+
+async function getBlog(id) {
+  axios.get('https://jsonplaceholder.typicode.com/posts/' + id)
+    .then((result) => {
+      state.blog = result?.data
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+
+}
+
+
+</script>
+
+<template>
+  <div>
+    <!-- component -->
+    <div class="flex flex-wrap md items-center h-screen">
+      <div class="bg-white w-full md:w-1/2">
+        <div class="mx-32">
+          <h1 class="text-6xl font-bold mt-16">title : {{ state?.blog?.title }}</h1>
+
+
+          <!-- description -->
+          <div class="description w-full sm: md:w-2/3 mt-16 text-gray-500 text-sm">
+            description : {{ state?.blog?.body }}
+          </div>
+
+          <button class="uppercase mt-5 text-sm font-semibold hover:underline bg-gray-300 rounded-md px-4 py-2" @click="router.push('/')">برگشت
+          </button>
+        </div>
+      </div>
+      <div class="bg-red-600 w-full md:w-1/2 ">
+        <img src="https://source.unsplash.com/7H77FWkK_x4/1600x900" class="h-full w-full" alt="" />
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/components/blog.vue
+++ src/components/blog.vue
@@ -1,4 +1,6 @@
 <script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter();
 
 const props = defineProps({
   blogs: {
@@ -12,12 +14,12 @@ const props = defineProps({
 
 <template>
   <div>
-    <div v-for="blog in props.blogs" :key="blog.id" class="flex h-40 m-5 pb-3 cursor-pointer border-b-2">
-        <img src="../assets/images/bg.png" alt="blog" class="rounded-lg">
-        <div class="px-6">
-            <p class="text-md">{{blog.title}}</p>
-            <p class="text-sm  mt-4">{{blog.body}}</p>
-        </div>
+    <div v-for="blog in props.blogs" :key="blog.id" class="flex h-40 m-5 pb-3 cursor-pointer border-b-2" @click="router.push('/blog/'+blog.id)">
+      <img src="../assets/images/bg.png" alt="blog" class="rounded-lg">
+      <div class="px-6">
+        <p class="text-md">{{ blog.title }}</p>
+        <p class="text-sm  mt-4">{{ blog.body }}</p>
+      </div>
     </div>
   </div>
 </template>
