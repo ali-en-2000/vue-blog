c8ca5c35a4bc68191247e2c7f657695678189c13
Ali reza <a.rezamohmmadi369@gmail.com>
2022-12-08 01:31:11 +0330

    add blog component

---
+++ src/components/blog.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+const props = defineProps({
+  blogs: {
+    type: Object,
+    required: true,
+  },
+})
+
+
+</script>
+
+<template>
+  <div>
+    <div v-for="blog in props.blogs" :key="blog.id" class="flex h-40 m-5 pb-3 cursor-pointer border-b-2">
+        <img src="../assets/images/bg.png" alt="blog" class="rounded-lg">
+        <div class="px-6">
+            <p class="text-md">{{blog.title}}</p>
+            <p class="text-sm  mt-4">{{blog.body}}</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
